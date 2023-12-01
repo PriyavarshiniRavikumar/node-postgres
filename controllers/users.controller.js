2586a52293fe7dbd9bb5005996cc6b1f374dc3ba
PriyavarshiniRavikumar <varsharkg1@gmail.com>
2023-12-01 22:47:18 +0530

    users

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -31,6 +31,90 @@ const addUserController = async (req, res, next) => {
   }
 };
 
+//updating the userData
+const updateUserController = async (req, res) => {
+  try {
+    const updateUser = await models.users.update(
+      {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        user_name: req.body.user_name,
+        user_password: req.body.user_password,
+        phone_no: req.body.phone_no,
+      },
+      {
+        where: {
+          user_id: req.params.id,
+        },
+        returning: true,
+      }
+    );
+
+    res.json({
+      updateUser,
+    });
+  } catch (error) {
+    return res.send({
+      message: error.errors.map((d) => d.message),
+    });
+  }
+};
+
+// login
+const loginController = async (req, res, next) => {
+  try {
+    const searchUser = await models.users.findAndCountAll({
+      //attributes: ["email", "user_name"],
+      where: {
+        user_name: req.body.user_name,
+        user_password: req.body.user_password,
+      },
+      returning: true,
+    });
+
+    if (searchUser.count == 0) {
+      return next({
+        status: 400,
+        message: "user not found, check the email and username",
+      });
+    } else {
+      res.json({
+        searchUser,
+      });
+    }
+  } catch (error) {
+    return res.send(error);
+  }
+};
+
+// view the account details
+const getAccountController = async (req, res, next) => {
+  //"select * from account_users au where id = $1";
+  try {
+    const getUserController = await models.users.findOne({
+      where: {
+        user_id: req.params.id,
+      },
+      returning: true,
+    });
+
+    res.json({
+      getUserController,
+    });
+  } catch (error) {
+    return next({
+      status: 400,
+      message: "unknown user id",
+    });
+  }
+};
+
+
 module.exports = {
   addUserController,
+  updateUserController,
+  loginController,
+  getAccountController,
+
 };
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,8 +3,29 @@ const router = express.Router();
 const { validate } = require("../middlewares/validate.middleware");
 
 const { addUserController } = require("../controllers/users.controller");
+const {
+    getUsers,
+    updateUserController,
+    loginController,
+    getAccountController,
+  } = require("../controllers/users.controller");
 const { signUpSchema } = require("../validations/authentication.schema");
+const {
+    
+    updateUserSchema,
+    loginSchema,
+  } = require("../validations/authentication.schema");
+  
 
 router.post("/signup", validate(signUpSchema), addUserController);
+//LOGIN
+router.post("/login", validate(loginSchema), loginController);
+
+//UPDATE USER DATA
+router.put("/user/:id", validate(updateUserSchema), updateUserController);
+
+//VIEW THE USER DATA
+router.get("/user/:id", getAccountController);
+
 
 module.exports = router;
