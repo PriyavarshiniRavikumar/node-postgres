66e9bc118e60382f8d304752da42b78d59dec737
PriyavarshiniRavikumar <varsharkg1@gmail.com>
2023-12-03 21:17:21 +0530

    items routes

--- routes/items.routes.js
+++ routes/items.routes.js
@@ -1,5 +1,31 @@
 const express = require("express");
 const router = express.Router();
-const { addItemController } = require("../controllers/items.controller");
+const { addItemController,
+    updateitemController,
+    getallitemcontroller,
+    getbysingleitemcontroller,
+    filterItemPricecontroller,
+    SearchItemNamecontroller,
+    sortItemNameController,
+    sortItemPriceController,
+   } = require("../controllers/items.controller");
+   const {
+    addfavoritescontroller,
+  } = require("../controllers/favourites.controller");
+  const { validate } = require("../middlewares/validate.middleware");
+  const {
+    itemaddSchema,
+    updateitemSchema,
+  } = require("../validations/items.schema");
+  
+  
 router.post("/add-items", addItemController);
+router.get("/items", getallitemcontroller);
+router.get("/get-one-item", getbysingleitemcontroller);
+router.post("/favourites", addfavoritescontroller);
+router.get("/filter", filterItemPricecontroller);
+router.get("/search", SearchItemNamecontroller);
+router.get("/sort-by-name", sortItemNameController);
+router.get("/sort-by-price", sortItemPriceController);
+
 module.exports = router;
